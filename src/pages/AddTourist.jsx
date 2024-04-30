6f04b926079b34451eb565395b30e7ffb07358e5
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-04-30 17:40:56 +0600

    Add Tourists Spots section Option tag implemented

--- src/pages/AddTourist.jsx
+++ src/pages/AddTourist.jsx
@@ -8,8 +8,8 @@ const AddTourist = () => {
   const [readOnly] = useState(true);
   const successNotify = () => toast.success("Successfully Added!");
 
-  const handleForm = event => {
-    event.preventDefault()
+  const handleForm = (event) => {
+    event.preventDefault();
     const form = event.target;
     const img_url = form.img_url.value;
     const spotName = form.spotName.value;
@@ -20,31 +20,43 @@ const AddTourist = () => {
     const season = form.season.value;
     const travelTime = form.travelTime.value;
     const totalVisitors = form.totalVisitors.value;
-    const email = user?.email ;
+    const email = user?.email;
     const userName = user?.displayName;
-    const spotInfo = {img_url, spotName, countryName, location, description, cost, season, travelTime, totalVisitors, email, userName}
-    // console.log(spotInfo)
+    const spotInfo = {
+      img_url,
+      spotName,
+      countryName,
+      location,
+      description,
+      cost,
+      season,
+      travelTime,
+      totalVisitors,
+      email,
+      userName,
+    };
+    console.log(spotInfo)
 
-    fetch('https://b9a10-server-side-muinuddin-sm12.vercel.app/spot-info', {
+    fetch("https://b9a10-server-side-muinuddin-sm12.vercel.app/spot-info", {
       method: "POST",
       headers: {
-        'content-type' : 'application/json'
+        "content-type": "application/json",
       },
-      body: JSON.stringify(spotInfo)
+      body: JSON.stringify(spotInfo),
     })
-    .then(res => res.json())
-    .then(() => {
-      // console.log(data)
-      form.reset();
-      successNotify()
-
-    })
-  }
+      .then((res) => res.json())
+      .then(() => {
+        // console.log(data)
+        form.reset();
+        successNotify();
+      });
+  };
 
   return (
     <div className="max-w-[1036px] mx-auto px-4 md:px-10 my-20">
       <section className="p-6 my-14 ">
-        <form onSubmit={handleForm}
+        <form
+          onSubmit={handleForm}
           noValidate=""
           action=""
           className="container flex flex-col mx-auto space-y-12"
@@ -76,13 +88,19 @@ const AddTourist = () => {
               </div>
               <div className="col-span-full sm:col-span-2">
                 <label className="text-sm">Country name</label>
-                <input
-                  type="text"
+                <select
                   name="countryName"
-                  placeholder="country name"
-                  className="w-full py-2 pl-2 outline-none border rounded-md "
+                  className="w-full py-2 pl-2 outline-none border rounded-md"
                   required
-                />
+                >
+                  <option value="">Select a season</option>
+                  <option value="United States of America">United States of America</option>
+                  <option value="Canada">Canada</option>
+                  <option value="Brazil">Brazil</option>
+                  <option value="Argentina">Argentina</option>
+                  <option value="Mexico">Mexico</option>
+                  <option value="Peru">Peru</option>
+                </select>
               </div>
               <div className="col-span-full sm:col-span-2">
                 <label className="text-sm">Location</label>
@@ -109,27 +127,30 @@ const AddTourist = () => {
                 <input
                   type="text"
                   name="cost"
-                  placeholder="average cost"
+                  placeholder="00.00"
                   className="w-full py-2 pl-2 outline-none border rounded-md "
                   required
                 />
               </div>
               <div className="col-span-full sm:col-span-2">
                 <label className="text-sm">Seasonality</label>
-                <input
-                  type="text"
+                <select
                   name="season"
-                  placeholder="summer/winter"
-                  className="w-full py-2 pl-2 outline-none border rounded-md "
+                  className="w-full py-2 pl-2 outline-none border rounded-md"
                   required
-                />
+                >
+                  <option value="">Select a season</option>
+                  <option value="Summer">Summer</option>
+                  <option value="Winter">Winter</option>
+                  <option value="Both">Both</option>
+                </select>
               </div>
               <div className="col-span-full sm:col-span-2">
                 <label className="text-sm">Travel time</label>
                 <input
                   type="text"
                   name="travelTime"
-                  placeholder="days"
+                  placeholder="0 days"
                   className="w-full py-2 pl-2 outline-none border rounded-md "
                   required
                 />
@@ -138,7 +159,7 @@ const AddTourist = () => {
                 <label className="text-sm">Total visitors per year</label>
                 <input
                   type="text"
-                  placeholder="total visitors"
+                  placeholder="0/million"
                   name="totalVisitors"
                   className="w-full py-2 pl-2 outline-none border rounded-md "
                   required
@@ -166,7 +187,11 @@ const AddTourist = () => {
                   readOnly
                 />
               </div>
-              <input className="btn col-span-2 lg:col-span-1 bg-[#00BA9C] text-white font-medium" type="submit" value="Add" />
+              <input
+                className="btn col-span-2 lg:col-span-1 bg-[#00BA9C] text-white font-medium"
+                type="submit"
+                value="Add"
+              />
             </div>
           </fieldset>
         </form>
